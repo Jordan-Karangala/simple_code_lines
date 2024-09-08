6fb665d83ddc7b5273fbe94fb65695f798beb475
Jordan-Karangala <121640771+Jordan-Karangala@users.noreply.github.com>
2024-09-08 14:44:41 +0530

    Create main.py
    This is where the main code snippets present, should you recheck while coding.

---
+++ main.py
@@ -0,0 +1,9 @@
+# To install python versions with pyenv
+pyenv install <python_version>
+
+# To create virtual venv with pyenv
+pyenv virtualenv <python_version> <venv_name>
+
+#  To display the list of python versions available to download
+pyenv install --list
+
